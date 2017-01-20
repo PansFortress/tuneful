4f08ed43d1a6c1d2e149ce97d5eb8ca7bf338d2d
PansFortress <Japan8789@gmail.com>
2017-01-19 22:56:47 -0500

    initial setup of models for song and file with some insertion errors

--- tuneful/models.py
+++ tuneful/models.py
@@ -5,5 +5,29 @@ from sqlalchemy import Column, Integer, String, Sequence, ForeignKey
 from sqlalchemy.orm import relationship
 
 from tuneful import app
-from .database import Base, engine
+from .database import Base, engine, session
 
+class Song(Base):
+    __tablename__ = "songs"
+    id = Column(Integer, primary_key=True)
+    file_id = Column(Integer, ForeignKey('files.id'))
+
+    def as_dictionary(self):
+        song = {
+            "id": self.id,
+            "file": self.file_id.as_dictionary()
+        }
+        return song
+
+class File(Base):
+    __tablename__ = "files"
+    id = Column(Integer, primary_key=True)
+    filename = Column(String(150))
+    song = relationship("Song", uselist=False, backref="file")
+
+    def as_dictonary(self):
+        file ={
+            "id": self.id,
+            "name": self.filename
+        }
+        return file
